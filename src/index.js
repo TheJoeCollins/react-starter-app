21ade0c17a15878b8606dfd774727d148bcba601
Joseph Collins <josephecollins1992@gmail.com>
2018-06-18 14:59:46 -0400

    React Start Package

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
-const data = {
-  name: "hello",
-  red: "red"
+
+//Object Spread operator and destructing
+const person = {
+  name: {
+    first: "Joe",
+    last: "Collins"
+  },
+  likes: ["Soccer", "Food", "Programming"],
+  title: "Sir"
+};
+console.log("Person: ", person);
+
+const { likes } = person;
+console.log("Likes: ", likes);
+
+const newLikes = [...likes, "Football", "Surfing"];
+
+//What ever is added last will override whats before it
+var newPerson = { ...person, likes: [...newLikes] };
+console.log("New Likes: ", newLikes);
+
+//New Person and the old person object
+console.log("New Person: ", newPerson);
+console.log("Original Person: ", person);
+
+//Computed Names
+const property = "age";
+var newPerson = { ...newPerson, [property]: 25 };
+console.log("Propert with age: ", newPerson);
+
+const getData = () => {
+  return new Promise((resolve, reject) => {});
 };
 
-const newData = { ...data };
-console.log(newData);
+//Named Export
 export default class App extends React.Component {
   render() {
     return (
