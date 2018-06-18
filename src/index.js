37d2200ad7c60407f253259c7d48d0c778cdcf4f
Joseph Collins <josephecollins1992@gmail.com>
2018-06-18 15:08:25 -0400

    Clean Starter

--- src/index.js
+++ src/index.js
@@ -1,35 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-//Object Spread operator and destructing
-const person = {
-  name: {
-    first: "Joe",
-    last: "Collins"
-  },
-  likes: ["Soccer", "Food", "Programming"],
-  title: "Sir"
-};
-console.log("Person: ", person);
-
-const { likes } = person;
-console.log("Likes: ", likes);
-
-const newLikes = [...likes, "Football", "Surfing"];
-
-//What ever is added last will override whats before it
-var newPerson = { ...person, likes: [...newLikes] };
-console.log("New Likes: ", newLikes);
-
-//New Person and the old person object
-console.log("New Person: ", newPerson);
-console.log("Original Person: ", person);
-
-//Computed Names
-const property = "age";
-var newPerson = { ...newPerson, [property]: 25 };
-console.log("Propert with age: ", newPerson);
-
 const getData = () => {
   return new Promise((resolve, reject) => {});
 };
