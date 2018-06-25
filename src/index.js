653a69f3f567a975cf03153dc96ff869abfce6cd
Joseph Collins <josephecollins1992@gmail.com>
2018-06-25 17:04:51 -0400

    React Starter Pack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./style.css";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -9,8 +10,10 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1> Hello World </h1>
+      <div className="flex-container">
+        <div className="flex-one">
+          <h1 className="hello"> Hello World </h1>
+        </div>
       </div>
     );
   }
