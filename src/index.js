219e438a806652cb437c34f40b2a929cf9e103fd
Joseph Collins <josephecollins1992@gmail.com>
2018-06-25 15:11:40 -0400

    Start App with plugins

--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider, connect } from "react-redux";
-import { applyMiddleware, createStore } from "redux";
-import { getTopStories } from "./reducers/hackernews/HackerReducer";
-import { HackerReducer } from "./reducers/hackernews/HackerReducer";
-import logger from "redux-logger";
-import thunk from "redux-thunk";
 
-//Sample Project Demo React-Redux
-//Hacker News Api - Real Time Data
-//React-Redux
-
-const middleware = applyMiddleware(thunk, logger);
-const store = createStore(HackerReducer, middleware);
-
-class App extends React.Component {
+export default class App extends React.Component {
   constructor(props) {
     super(props);
   }
-  componentDidMount = () => {
-    this.props.getStories();
-  };
+  componentDidMount = () => {};
 
   render() {
     return (
@@ -31,28 +16,4 @@ class App extends React.Component {
   }
 }
 
-const mapState = state => {
-  return {
-    loading: state.loading,
-    topStories: state.topStoriesIds
-  };
-};
-const mapDisp = dispatch => {
-  return {
-    getStories: () => {
-      dispatch(getTopStories());
-    }
-  };
-};
-
-const AppMain = connect(
-  mapState,
-  mapDisp
-)(App);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <AppMain />
-  </Provider>,
-  document.querySelector("#main")
-);
+ReactDOM.render(<AppMain />, document.querySelector("#main"));
